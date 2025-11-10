3db7c4089d70bf7bca58c0086529a952c72cec44
yaakov-k613 <226129952+yaakov-k613@users.noreply.github.com>
2025-11-10 21:01:30 +0000

    Change personaltop to personalfullstack

--- personalfullstack/js_personalfullstack/script_personalfullstack.js
+++ personalfullstack/js_personalfullstack/script_personalfullstack.js
@@ -1,6 +1,8 @@
 document.getElementById("openOdin").onclick = () =>
   window.open("https://www.theodinproject.com", "_blank");
 
+document.getElementById("openFreeCodeCamp").onclick = () =>
+  window.open("https://www.freecodecamp.org", "_blank");
+
 document.getElementById("openWorkflowy").onclick = () =>
   window.open("https://workflowy.com", "_blank");
-
