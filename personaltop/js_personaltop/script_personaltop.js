caf8e777beecaeda111837655d434c1d94c91f9a
yaakov-k613 <226129952+yaakov-k613@users.noreply.github.com>
2025-11-10 20:17:22 +0000

    Add html, css, and js for initial commit for personaltop for the Odin Project

---
+++ personaltop/js_personaltop/script_personaltop.js
@@ -0,0 +1,5 @@
+document.getElementById("odinBtn").onclick = () =>
+  document.getElementById("embeddedPage").src = "https://www.theodinproject.com";
+
+document.getElementById("workflowyBtn").onclick = () =>
+  document.getElementById("embeddedPage").src = "https://workflowy.com";
